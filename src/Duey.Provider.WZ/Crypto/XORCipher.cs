0694ae1b1e7fbe7e85d888bf8e1d0df9da9aaafe
Kaioru <keith@kaioru.co>
2023-10-11 14:48:33 +0800

    Fix XOR key not preparing properly

--- src/Duey.Provider.WZ/Crypto/XORCipher.cs
+++ src/Duey.Provider.WZ/Crypto/XORCipher.cs
@@ -10,19 +10,19 @@ public class XORCipher
     public XORCipher() : this(WZImageIV.GMS)
     {
     }
-    
+
     public XORCipher(IReadOnlyList<byte> iv) : this(new AESCipher(), iv)
     {
     }
-    
+
     public XORCipher(AESCipher aes, IReadOnlyList<byte> iv)
     {
         _aes = aes;
         _key = new[]
         {
-            iv[0], iv[1], iv[2], iv[3], 
-            iv[0], iv[1], iv[2], iv[3], 
-            iv[0], iv[1], iv[2], iv[3], 
+            iv[0], iv[1], iv[2], iv[3],
+            iv[0], iv[1], iv[2], iv[3],
+            iv[0], iv[1], iv[2], iv[3],
             iv[0], iv[1], iv[2], iv[3]
         };
         _key = aes.Transform(_key);
@@ -31,27 +31,28 @@ public class XORCipher
     private void Prepare(int requiredLength)
     {
         if (_key.Length > requiredLength) return;
-        
+
         var newSize = requiredLength / 16 + 1;
         var previousSize = _key.Length;
-        
-        newSize *= 2;
+
+        newSize *= 16;
 
         Array.Resize(ref _key, newSize);
 
         for (var offset = previousSize; offset < newSize; offset += 16)
         {
-            var result = _aes.Transform(_key[offset..(offset + 16)]);
-            result.CopyTo(_key, offset);
+            var result = _aes.Transform(_key[(offset - 16)..offset]);
+
+            Array.Copy(result, 0, _key, offset, 16);
         }
     }
 
     public void Transform(byte[] input)
     {
         var inputLength = input.Length;
-        
+
         Prepare(inputLength);
-        
+
         const int bigChunkSize = sizeof(ulong);
 
         unsafe
@@ -67,7 +68,7 @@ public class XORCipher
                 var intBlocks = inputLength / bigChunkSize;
                 for (; i < intBlocks; ++i)
                 {
-                    *(ulong*) currentInputByte ^= *(ulong*) currentXorByte;
+                    *(ulong*)currentInputByte ^= *(ulong*)currentXorByte;
                     currentInputByte += bigChunkSize;
                     currentXorByte += bigChunkSize;
                 }
